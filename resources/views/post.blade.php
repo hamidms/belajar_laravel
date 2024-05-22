0bbacce21d48c051e0816a113cdfce288e6a1067
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-05-22 22:09:45 +0700

    add post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,14 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    @foreach ($posts as $post)
-        
     <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <a href="/posts/{{ $post['id'] }}" class="hover:underline">
             <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-        </a>
+
         <div class="text-base text-gray-500">
             <a href="#">{{ $post['author'] }}</a> | 22 May 2024
         </div>
-        <p class="my-4 font-light">{{ Str::limit($post['body'], 120) }}</p>
-        <a href="/posts/{{ $post['id'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo</a>
+        <p class="my-4 font-light">{{ $post['body'] }}</p>
+        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo Back to posts</a>
     </article>
-    @endforeach
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,11 +14,13 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
         [
+            'id' => 1,
             'title' => 'Judul Artikel 1',
             'author' => 'Hamid Machfudin Sukardi',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptates eius deserunt accusamus, doloribus repellendus? Hic temporibus, doloribus quis voluptas harum officia! Animi omnis autem nihil veritatis aut eos veniam.'
         ],
         [
+            'id' => 2,
             'title' => 'Judul Artikel 2',
             'author' => 'Hamid Machfudin Sukardi',
             'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non tempore cum exercitationem perspiciatis necessitatibus sit saepe laboriosam eos cupiditate enim laborum nemo ducimus repellat tempora obcaecati qui recusandae, voluptatem atque porro explicabo!'
@@ -25,6 +28,31 @@ Route::get('/posts', function () {
     ]]);
 });
 
+Route::get('/posts/{id}', function($id) {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Hamid Machfudin Sukardi',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptates eius deserunt accusamus, doloribus repellendus? Hic temporibus, doloribus quis voluptas harum officia! Animi omnis autem nihil veritatis aut eos veniam.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Hamid Machfudin Sukardi',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non tempore cum exercitationem perspiciatis necessitatibus sit saepe laboriosam eos cupiditate enim laborum nemo ducimus repellat tempora obcaecati qui recusandae, voluptatem atque porro explicabo!'
+        ],
+    ];
+
+    $post = Arr::first($posts, function($post) use ($id) {
+        return $post['id'] == $id;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
+
+
