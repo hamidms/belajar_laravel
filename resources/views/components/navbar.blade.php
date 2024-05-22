03def4736133547f8f9aecfa827886753ac9f8b7
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-05-22 21:50:21 +0700

    add component

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,10 +8,11 @@
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-              <a href="/blog" class="{{ request()->is('blog') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">Blog</a>
-              <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
-              <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+              <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+              <x-nav-link href="/posts" :active="request()->is('posts')">Blog</x-nav-link>
+              <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+              <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+              
             </div>
           </div>
         </div>
@@ -69,10 +70,10 @@
     <div x-show="isOpen" class="md:hidden" id="mobile-menu">
       <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
-        <a href="/blog" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Blog</a>
-        <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
-        <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
+        <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+        <x-nav-link href="/posts" :active="request()->is('posts')">Blog</x-nav-link>
+        <x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
+        <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
       </div>
       <div class="border-t border-gray-700 pb-3 pt-4">
         <div class="flex items-center px-5">
--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,22 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Hamid Sukardi']);
+    return view('about', ['nama' => 'Hamid Sukardi', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Hamid Machfudin Sukardi',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptates eius deserunt accusamus, doloribus repellendus? Hic temporibus, doloribus quis voluptas harum officia! Animi omnis autem nihil veritatis aut eos veniam.'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Hamid Machfudin Sukardi',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non tempore cum exercitationem perspiciatis necessitatibus sit saepe laboriosam eos cupiditate enim laborum nemo ducimus repellat tempora obcaecati qui recusandae, voluptatem atque porro explicabo!'
+        ],
+    ]]);
 });
 
 Route::get('/contact', function () {
