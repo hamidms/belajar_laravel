cf0d077cd15c7dacba8400857a8ea8c154169a11
Hamid Sukardi <hamid@Hamids-MBP.bbrouter>
2024-05-26 22:15:03 +0700

    create a post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,36 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Hamid Machfudin Sukardi',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio voluptates eius deserunt accusamus, doloribus repellendus? Hic temporibus, doloribus quis voluptas harum officia! Animi omnis autem nihil veritatis aut eos veniam.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Judul Artikel 2',
-                'slug' => 'judul-artikel-2',
-                'author' => 'Hamid Machfudin Sukardi',
-                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non tempore cum exercitationem perspiciatis necessitatibus sit saepe laboriosam eos cupiditate enim laborum nemo ducimus repellat tempora obcaecati qui recusandae, voluptatem atque porro explicabo!'
-            ],
-        ];
-    }
+    use HasFactory;
     
-    public static function find($slug): array {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404);
-        };
-        
-        return $post;
-    }
+    protected $fillable = [
+        'title',
+        'author',
+        'slug',
+        'body'
+    ];
 }
